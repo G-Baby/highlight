49d27cdd6c40279614d8c5cceaa556a018948a20
G-Baby <gpw10097@saintjoe.edu>
2015-04-10 11:14:39 -0500

    Update highlightCode.js

--- js/highlightCode.js
+++ js/highlightCode.js
@@ -1,12 +1,12 @@
-function highlightCode(node, keywords) {
-  var text = node.textContent;
+function highlightCode(node, keywords) { //the name and fuction
+  var text = node.textContent; //this makes the variable text
   node.textContent = ""; // Clear the node
 
-  var match, pos = 0;
-  while (match = keywords.exec(text)) {
-    var before = text.slice(pos, match.index);
-    node.appendChild(document.createTextNode(before));
-    var strong = document.createElement("strong");
+  var match, pos = 0; //sets the pointer to position 0
+  while (match = keywords.exec(text)) {  //starts the while loop. Matches text with keywords
+    var before = text.slice(pos, match.index); //
+    node.appendChild(document.createTextNode(before)); //creates node
+    var strong = document.createElement("strong"); //
     strong.appendChild(document.createTextNode(match[0]));
     node.appendChild(strong);
     pos = keywords.lastIndex;
